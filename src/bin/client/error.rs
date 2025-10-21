7d78d2182369364b135e2ef251b9398819ef7ace
Tomas Perez Dangelo <tomiperez@192.168.1.3>
2025-10-21 11:07:01 -0300

    Little change in client's errors

--- src/bin/client/error.rs
+++ src/bin/client/error.rs
@@ -9,8 +9,8 @@ pub enum ClientError {
 impl Display for ClientError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         match self {
-            ClientError::SdpCreationError(e) => write!(f, "Error: \"could not create SDP\". Details: {}", e),
-            ClientError::IceConnectionError(e) => write!(f, "Error: \"could not connect to ice connection\". Details: {}", e),
+            ClientError::SdpCreationError(e) => write!(f, "{}", e),
+            ClientError::IceConnectionError(e) => write!(f, "{}", e),
         }
     }
 }

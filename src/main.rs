3b5e3cf5ef74135b84211091806e353fb0dcb3ca
Tomas Perez Dangelo <tomiperez@MacBook-Air-de-Tomas.local>
2025-11-03 01:07:05 -0300

    path to config hardcoded

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,26 @@ use roomrtc::config::Config;
 use roomrtc::ui::interface::RoomRTCApp;
 
 fn main() -> Result<(), eframe::Error> {
+    /*
     let args: Vec<String> = env::args().collect();
+
     if args.len() != 2 {
         eprintln!("Usage: {} /path/to/roomrtc.conf", args[0]);
         std::process::exit(1);
     }
-    let config_path = &args[1];
+    let config_path = Path::new(&args[1]);
 
-    let config = Config::load(Path::new(config_path))
-        .expect("Failed to load configuration file. Make sure it exists and is valid.");
+     */
+
+    let config_path = Path::new("roomrtc.conf");
+
+    let config = match Config::load(config_path) {
+        Ok(cfg) => cfg,
+        Err(e) => {
+            eprintln!("Failed to load config from {}: {}", config_path.display(), e);
+            std::process::exit(1);
+        }
+    };
 
     let options = eframe::NativeOptions::default();
     eframe::run_native(
